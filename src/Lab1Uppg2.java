c5c329b1239838a24c69bfb8a2793154ab69d9f3
Jonas Sjöberg <jomeganas@gmail.com>
2015-06-09 01:49:59 +0200

    Uppgift 2 färdig + eclipse dotfiles.

--- src/Lab1Uppg2.java
+++ src/Lab1Uppg2.java
@@ -12,58 +12,48 @@
 import java.util.Scanner;
 
 public class Lab1Uppg2 {
-	
-	/* Konstant värde, typ #define i C-liknande språk med en pre-processor. 
-	 * ITERATIONS är antalet tal som ska matas in. Konstant vid exekvering. */
-	private static final int ITERATIONS = 9;
-	private static final String[] TALORD_OBES = { "noll", "ett", "två", "tre",
-											      "fyra", "fem", "sex", "sju",
-											      "åtta", "nio", "tio", "osv" };
+    
+    /* Konstant värde, typ #define i C-liknande språk med en pre-processor. 
+     * ITERATIONS är antalet tal som ska matas in. Konstant vid exekvering. */
+    private static final int ITERATIONS = 3;
 
-	private static final String[] TALORD_BEST = { "nollte", "första", "andra", 
-											      "tredje", "fjärde", "femte",
-											      "sjätte", "sjunde", "åttonde",
-											      "nionde", "tionde", "osv" };
+    /* Svenska Räkneord i string-arrays. Arrayindex överenstämmer med talet. */
+    private static final String[] GRUNDTAL = { "noll", "ett", "två", "tre",
+                                               "fyra", "fem", "sex", "sju",
+                                               "åtta", "nio", "tio", "osv" };
 
-	public static void main(String[] args) {
-		/* Deklarara variabler och nytt Scanner-objekt för att läsa I/O. */
-		Scanner scan = new Scanner(System.in);
-		int input, sum = 0;
-		double average = 0;
-		long product = 1; /* Kan räkna med att produkten kan bli väldigt stor */
-		
-		/* Fråga efter 'ITERATIONS' antal tal. Pussla ihop strängar och hätma 
-		 * tal från arrayen 'TALORD_OBES'. */
-		System.out.println("Skriv in " + TALORD_OBES[ITERATIONS] + " heltal.");
-		
-		/* */
-		for (int i = 1; i < ITERATIONS + 1; i++) {
-			System.out.print("Skriv in det " + TALORD_BEST[i] + " talet: ");
-			input = scan.nextInt();
-			
-			sum += input;
-			//System.out.println("Sum: " + sum);
+    private static final String[] ORDNINGSTAL = { "nollte", "första", "andra", 
+                                                  "tredje", "fjärde", "femte",
+                                                  "sjätte", "sjunde", "åttonde",
+                                                  "nionde", "tionde", "osv" };
 
-			average = sum / i;
-			
-			//System.out.println("Average: " + average);
+    
+    public static void main(String[] args) {
+        /* Deklarara variabler och nytt Scanner-objekt för att läsa I/O. */
+        Scanner scan = new Scanner(System.in);
+        int input, sum = 0;
+        double average = 0;
+        long product = 0; /* Kan räkna med att produkten kan bli väldigt stor */
+        
+        /* Fråga efter 'ITERATIONS' antal tal. Pussla ihop strängar och hämta 
+         * svenska räkneord från arrayen 'GRUNDTAL'. */
+        System.out.println("Skriv in " + GRUNDTAL[ITERATIONS] + " heltal.");
+    
+        /* Loopa 'ITERATIONS' gånger, fråga efter talen i sekventiell följd.
+         * Om noll heltal efterfrågas avslutas programmet direkt. */
+        for (int i = 1; i < ITERATIONS + 1; i++) {
+            System.out.print("Skriv in det " + ORDNINGSTAL[i] + " talet: ");
 
-			product = product * input;
-			//System.out.println("Product: " + product);
-		}
-		
-		System.out.println("Summan av talen är " 	  + sum
-					   + "\nMedelvärdet av talen är " + average
-					   + "\nProdukten av talen är "   + product);
-		
-		
-//		Skriv in tre heltal.
-//		Skriv in det första talet: 20
-//		Skriv in det andra talet: 30
-//		Skriv in det tredje talet: 25
-//		Summan av talen är 75.
-//		Medelvärdet av talen är 25.
-//		Produkten av talen är 15000.
-	}
-
+            input = scan.nextInt();
+            
+            sum += input;
+            average = sum / i;
+            product = product * input;
+        }
+        
+        /* Skriv ut resultatet till stdout. */
+        System.out.println("Summan av talen är "      + sum
+                       + "\nMedelvärdet av talen är " + average
+                       + "\nProdukten av talen är "   + product);
+    }
 }
