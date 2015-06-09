2a009f6839a4aac81ca14cf62271f2b526088a04
Jonas Sjöberg <jomeganas@gmail.com>
2015-06-09 02:35:15 +0200

    Uppgift 3 KLAR.

--- src/Lab1Uppg3.java
+++ src/Lab1Uppg3.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /* 
  *  DV017A :: Grundläggande programmering i Java
  *  ============================================
@@ -12,7 +14,17 @@
 public class Lab1Uppg3 {
 
 	public static void main(String[] args) {
+        /* Skapa Scanner-objekt för att läsa I/O. Ignorera IDE:ns varningar. */
+        @SuppressWarnings("resource")
+		Scanner scan = new Scanner(System.in);	
+		
+        System.out.println("Program som konverterar mil till km.");
+        
+        System.out.print("Skriv in antal mil: ");
+        double mil = scan.nextDouble();
 
+        double kilometer = mil * 1000;
+        
+        System.out.println("Motsvarande antal km: " + kilometer);
 	}
-
 }
