3ede3b25ef7d37b85d68574a629cb83466adc5e7
Jonas Sjöberg <jomeganas@gmail.com>
2015-06-09 04:28:54 +0200

    Tabs -> spaces och ta bort whitespace.

--- src/Lab1Uppg1.java
+++ src/Lab1Uppg1.java
@@ -1,43 +1,43 @@
-/* 
+/*
  *  DV017A :: Grundläggande programmering i Java
  *  ============================================
  *  Uppdaterad 2015-06-09
  *  Jonas Sjöberg, Högskolan i Gävle.
  *  <tel12jsg@student.hig.se>
- *  
+ *
  *  Labb #1
  *  Uppgift 1
  */
 
 public class Lab1Uppg1 {
-	public static void main(String[] args) {
-		boolean data1 = true;
-		float data2 = 45.8F;
-		int data3 = 29;
-		boolean data4 = data3 < 10;
+    public static void main(String[] args) {
+        boolean data1 = true;
+        float data2 = 45.8F;
+        int data3 = 29;
+        boolean data4 = data3 < 10;
 
-		/* Även om data5 skulle vara av typen double som klarar decimaltal,
-		 * så är 12 och 5 båda av typen int per default och decimaldelen 
-		 * går förlorad. Därmed kan data5 ha typen 'int. */
-		int data5 = 12 / 5;
-		
-		/* Då varken data3 eller data5 är decimaltal kan int också användas. */ 
-		double data6 = data3 * data5;
+        /* Även om data5 skulle vara av typen double som klarar decimaltal,
+         * så är 12 och 5 båda av typen int per default och decimaldelen
+         * går förlorad. Därmed kan data5 ha typen 'int. */
+        int data5 = 12 / 5;
 
-		int data7 = 10 % 3;
-		String data8 = "Java programmering";
-		char data9 = 'b';
-		float data10 = (float) data5 / 4;
+        /* Då varken data3 eller data5 är decimaltal kan int också användas. */
+        double data6 = data3 * data5;
 
-		System.out.println("Variabeln data1:  " + data1
-		               + "\nVariabeln data2:  " + data2
-		               + "\nVariabeln data3:  " + data3
-		               + "\nVariabeln data4:  " + data4
-		               + "\nVariabeln data5:  " + data5
-		               + "\nVariabeln data6:  " + data6
-		               + "\nVariabeln data7:  " + data7
-		               + "\nVariabeln data8:  " + data8
-		               + "\nVariabeln data9:  " + data9
-		               + "\nVariabeln data10: " + data10);
-	}
+        int data7 = 10 % 3;
+        String data8 = "Java programmering";
+        char data9 = 'b';
+        float data10 = (float) data5 / 4;
+
+        System.out.println("Variabeln data1:  " + data1
+                       + "\nVariabeln data2:  " + data2
+                       + "\nVariabeln data3:  " + data3
+                       + "\nVariabeln data4:  " + data4
+                       + "\nVariabeln data5:  " + data5
+                       + "\nVariabeln data6:  " + data6
+                       + "\nVariabeln data7:  " + data7
+                       + "\nVariabeln data8:  " + data8
+                       + "\nVariabeln data9:  " + data9
+                       + "\nVariabeln data10: " + data10);
+    }
 }
--- src/Lab1Uppg3.java
+++ src/Lab1Uppg3.java
@@ -1,10 +1,10 @@
-/* 
+/*
  *  DV017A :: Grundläggande programmering i Java
  *  ============================================
  *  Uppdaterad 2015-06-09
  *  Jonas Sjöberg, Högskolan i Gävle.
  *  <tel12jsg@student.hig.se>
- *  
+ *
  *  Labb #1
  *  Uppgift 3
  */
@@ -13,25 +13,25 @@ import java.util.Scanner;
 
 public class Lab1Uppg3 {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
         /* Skapa Scanner-objekt för att läsa I/O. Ignorera IDE:ns varningar. */
         @SuppressWarnings("resource")
-		Scanner scan = new Scanner(System.in);	
-		
+        Scanner scan = new Scanner(System.in);
+
         System.out.println("Program som konverterar mil till km.");
-        
+
         System.out.print("Skriv in antal mil: ");
         double mil = scan.nextDouble();
 
         double kilometer = mil * 1000;
-        
+
         /* Skriv ut med 'format', ekvivalent med 'printf' som i sin tur känns
-         * igen från C-liknande språk. 
-         * 
-         * %.0f		Float with zero places after decimal point.		
-         * %n		A new line character appropriate to the platform running the
-         *          application. You should always use %n, rather than \n.  
+         * igen från C-liknande språk.
+         *
+         * %.0f     Float with zero places after decimal point.
+         * %n       A new line character appropriate to the platform running the
+         *          application. You should always use %n, rather than \n.
          */
         System.out.format("Motsvarande antal kilometer: %.0f%n", kilometer);
-	}
+    }
 }
