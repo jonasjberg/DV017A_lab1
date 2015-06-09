f77a19d5d3b18bf5a4869fd2f436a5205b21c2f9
Jonas Sjöberg <jomeganas@gmail.com>
2015-06-09 17:29:11 +0200

    Börja med #5.

---
+++ src/Lab1Uppg5.java
@@ -0,0 +1,19 @@
+/*
+ *  DV017A :: Grundläggande programmering i Java
+ *  ============================================
+ *  Uppdaterad 2015-06-09
+ *  Jonas Sjöberg, Högskolan i Gävle.
+ *  <tel12jsg@student.hig.se>
+ *
+ *  Labb #1
+ *  Uppgift 5
+ */
+
+public class Lab1Uppg5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
