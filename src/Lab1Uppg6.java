bcee8aaac0f691b4e575ead3a076f6dc488a836a
Jonas Sjöberg <jomeganas@gmail.com>
2015-06-11 07:47:25 +0200

    Lägg till kommentar i #6.

--- src/Lab1Uppg6.java
+++ src/Lab1Uppg6.java
@@ -1,5 +1,3 @@
-import java.util.Scanner;
-
 /*
  *  DV017A :: Grundläggande programmering i Java
  *  ============================================
@@ -11,10 +9,58 @@ import java.util.Scanner;
  *  Uppgift 6
  */
 
+import java.util.Scanner;
+
 public class Lab1Uppg6 {
 
+    private static final String QUERY = "Ange det heltal som du vill räkna ner från: ";
+
     public static void main(String[] args) {
+		/* Skapa Scanner-objekt för att läsa I/O. Ignorera IDE:ns varningar. */
+        @SuppressWarnings("resource")
+        Scanner scan = new Scanner(System.in);
 
+        /* Att 'start' får värdet -1 gör ingen faktiskt skillnad eftersom att
+         * 'scan' garanterar att den inre while-loopen inte avslutas förrän
+         * 'start' faktiskt håller en integer och do-loopens conditional kan
+         * utvärderas säkert.
+         */
+
+        int start = -1;
+
+        do {
+        /* Loopa tills inmatning är ett nollskiljt och positivt heltal. */
+            System.out.print(QUERY);
+
+            while (!scan.hasNextInt()) {
+                /* Loopa tills nästa "token" i scan's lista går att parsea till en
+                 * int. Avgränsare mellan "tokens" är whitespace som standard,
+                 * där whitespace är mellanslag och andra tecken som motsvarar
+                 * tomt vertikalt eller horisontellt utrymme i text.
+                 */
+                System.out.print(QUERY);
+                scan.next();
+            }
+
+            /* Scan måste hålla en int i sin "lista" som är OK att använda. */
+            start = scan.nextInt();
+
+        } while (start <= 0);
+
+
+        while (start > 0) {
+//        	if ((boolean) start % 2){
+//        		System.out.print(" ");
+//        	}
+            System.out.print(start + " ");
+            start--;
+        }
+
+//		Ange det heltal som du vill räkna ner från: 6
+//		6 5 4 3 2 1
+
+        /* Returnera lyckad exekvering oavsett hur det faktiskt gick. */
+        System.exit(0);
     }
-
 }
+
