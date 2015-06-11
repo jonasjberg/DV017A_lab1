49fadc73e56ebf28a6e8a0eaa3795035198dc55c
Jonas Sjöberg <jomeganas@gmail.com>
2015-06-11 07:26:58 +0200

    Börja på #6.

---
+++ src/Lab1Uppg6.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+/*
+ *  DV017A :: Grundläggande programmering i Java
+ *  ============================================
+ *  Uppdaterad 2015-06-11
+ *  Jonas Sjöberg, Högskolan i Gävle.
+ *  <tel12jsg@student.hig.se>
+ *
+ *  Labb #1
+ *  Uppgift 6
+ */
+
+public class Lab1Uppg6 {
+
+    public static void main(String[] args) {
+
+    }
+
+}
