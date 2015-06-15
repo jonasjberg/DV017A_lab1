96f4a1ab82faf7f729502c308ac25e9e1ccb951c
Jonas Sjöberg <jomeganas@gmail.com>
2015-06-15 17:09:07 +0200

    Börja #7.

---
+++ src/Lab1Uppg7.java
@@ -0,0 +1,28 @@
+
+public class Lab1Uppg7 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+//		7. Skriv ett program som låter användaren mata in värden till de tre
+//		   heltalsvariablerna var1, var2 och var3. Programmet skall sedan för varje
+//		   påstående a) – e) lagra värdet av påståendet i den booleska variabeln svar
+//		   och därefter skriva ut värdet av svar , försett med lämplig ledtext:
+//
+//		    a) Talet var1 är jämnt delbart med 7.
+//		    b) Talet var3 är inte jämnt delbart med talet var2.
+//		    c) Talet var1 är större än minst något av talen var2 och var3.
+//		    d) Talet var1 är större än talet var2, som i sin tur är större än talet var3.
+//		    e) Talet var1 är större än ett av talen var2 och var3, men inte större än båda.
+//
+//		   Tips: För att kolla om något tal är jämnt delbart med ett annat,
+//		         använd modulus-operatorn %!
+//	
+		
+		
+		
+	}
+
+}
